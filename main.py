526bb3d31a4e53703248fee4217241a695932007
NHAN <ntnhan.hk@gmail.com>
2021-05-03 16:46:51 +0900

    20210503

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("test the git")
-print("done test")
+import pandas_datareader as pdr
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+aapl = pdr.get_data_yahoo('AAPL',
+                          start=datetime.datetime(2016,4,1),
+#                          end=datetime.datetime(2021,4,30))
+                          end=datetime.date.today())
+aapl['Close'].plot(grid=True)
+plt.show()
