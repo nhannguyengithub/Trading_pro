9b5b0e61390373fa35392627756d92c680db97e9
nhan <ntnhan.hk@gmail.com>
2021-05-04 06:31:38 +0900

    210504 h

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("test the git")
-print("done test")
+import pandas_datareader as pdr
+import datetime
+import matplotlib.pyplot as plt
+
+aapl = pdr.get_data_yahoo('AAPL',
+                        start = datetime.datetime(2018,4,1),
+                        end = datetime.date.today())
+brk_b = pdr.get_data_yahoo('BRK-B',
+                           start=datetime.datetime(2018, 4, 1),
+                           end=datetime.date.today())
+
+aapl['Close'].plot(grid = True)
+brk_b['Close'].plot(grid = True)
+plt.show()
