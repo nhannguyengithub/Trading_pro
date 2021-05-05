54145d3d2794ac16155a50535ec65820092e75f1
nhan <ntnhan.hk@gmail.com>
2021-05-06 07:38:48 +0900

    210506 h

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-aapl = pdr.get_data_yahoo('AAPL',
-                        start = datetime.datetime(2018,4,1),
-                        end = datetime.date.today())
+def get(tickers, startday, endday):
+    def data(ticker):
+        return pdr.get_data_yahoo(ticker,start=startday,end=endday)
+    datas=map(data,tickers)
+    return pd.concat(datas,keys=tickers, names=['Ticker', 'Date'])
+
+tickers=['AAPL','MSFT','IBM','GOOG']
+all_data=get(tickers,datetime.datetime(2018,4,1),datetime.date.today())
+daily_close_px=all_data[['Adj Close']].reset_index().pivot('Date', 'Ticker', 'Adj Close')
+daily_pct_change_px=daily_close_px.pct_change()
+daily_pct_change_px.hist(bins=50, sharex=True, figsize=(12,8))
+plt.show()
+#aapl = pdr.get_data_yahoo('AAPL',
+#                        start = datetime.datetime(2018,4,1),
+#                        end = datetime.date.today())
 #brk_b = pdr.get_data_yahoo('BRK-B',
 #                           start=datetime.datetime(2018, 4, 1),
 #                           end=datetime.date.today())
@@ -17,23 +29,26 @@ aapl = pdr.get_data_yahoo('AAPL',
 #aapl.to_csv('aapl_data')
 #pd=pd.read_csv('aapl_data',header=0, index_col='Date', parse_dates=True)
 
-daily_close=aapl[['Adj Close']]
-daily_pct_change=daily_close.pct_change()
-daily_pct_change.fillna(0,inplace=True)
+#daily_close=aapl[['Adj Close']]
+#daily_pct_change=daily_close.pct_change()
+#daily_pct_change.fillna(0,inplace=True)
 #print(daily_pct_change)
 
-daily_log_returns=np.log(daily_pct_change+1)
+#daily_log_returns=np.log(daily_pct_change+1)
 #print(daily_log_returns)
 
-monthly=daily_close.resample('BM').apply(lambda x:x[-1])
-monthly_pct_change=monthly.pct_change()
+#monthly=daily_close.resample('BM').apply(lambda x:x[-1])
+#monthly_pct_change=monthly.pct_change()
 #print(aapl)
 #print(monthly_pct_change)
 #aapl.asfreq("M", method="bfill")
 #daily_pct_change.hist(bins=100)
 #plt.show()
 #print(daily_pct_change.describe())
-cum_daily_returns=(1+daily_pct_change).cumprod()
-print(cum_daily_returns)
-cum_daily_returns.plot()
-plt.show()
+#cum_daily_returns=(1+daily_pct_change).cumprod()
+#print(cum_daily_returns)
+#cum_daily_returns.plot()
+#cum_monthly_returns=cum_daily_returns.resample('BM').apply(lambda x:x[-1])
+#cum_quarter_returns=cum_monthly_returns.resample('4M').mean()
+#cum_quarter_returns.plot()
+#plt.show()
